abbdcf15b464df33867cf07913323c8709a558f8
sujanchhengutala9@gmail.com <sujanchhengutala9@gmail.com>
2021-06-18 19:58:10 +0545

    array methods

---
+++ 6-array method/script.js
@@ -0,0 +1,52 @@
+'use strict'
+const friends = ['rojan','kamal','pradip'];
+//////////////////////////////////////////////
+
+//1.push METHODS
+//friends.push('sarala','kiran');
+//const newLength = friends.push('Sarala','jojo');
+//console.log(friends);
+
+//////////////////////////////////////////////
+//2.unshift method
+//friends.unshift('nepal');
+//const newLength = friends.unshift('Rupak');
+//console.log(newLength);
+//console.log(friends);
+//////////////////////////////////////////////
+
+// 3. POP METHODS
+//friends.pop();
+// const popped = friends.pop();
+// console.log(popped);
+//console.log(friends);
+
+//////////////////////////////////////////////
+// 4. Shift METHOD
+//friends.shift()
+//console.log(friends);
+
+//////////////////////////////////////////////
+// 5. POSITION OF ELEMENT IN ARRAY
+//console.log(friends.indexOf('kamal'));
+// console.log(friends.indexOf('pradip'));
+
+//////////////////////////////////////////////
+// 6. MODERN POSITION METHOD
+//console.log(friends.includes('ram'));
+//console.log(friends.includes('kamal'));
+
+//////////////////////////////////////////////
+// 7. EXERCISE
+friends.push(23);
+console.log(friends);
+console.log(friends.includes('23'));
+// Strict equality. No type coercion.
+
+//////////////////////////////////////////////
+// 8. EXERCISE
+if(friends.includes('Rupak')){
+  console.log("You are friend with Rupak !");
+}
+// Used for conditional operatorconst friends = ['Abhilekh', 'Rupak', 'Saraswati'];
+//////////////////////////////////////////////
