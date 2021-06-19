992dd0708cc9118450c75858b1a073ec5e9d3bc8
sujanchhengutala9@gmail.com <sujanchhengutala9@gmail.com>
2021-06-19 12:00:38 +0545

    object

---
+++ object/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Multiple values all belongs to friends denoted by their position or order number but not name.
+
+const friends = {
+    friend1: 'pradip',
+    friend2: 'nepal',
+    age1 : '21',
+    age  : '22',
+    othersFriends: ['dinesh', 'rameshor']
+};
+console.log(friends);
+
+// key and values
+// key is also called property
+// order of the key values doesn't matter while retrieving
+// Array is structured data but object unstructured.
+
+// {} - object literal syntax
+//console.log(console);
+// console.log(console.log);
